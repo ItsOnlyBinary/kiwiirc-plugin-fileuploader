2488565756167a25b4121d5c985634324028169c
ItsOnlyBinary <git@itsonlybinary.com>
2022-08-09 18:22:28 +0100

    Update client plugin

--- fileuploader-kiwiirc-plugin/src/token-manager.js
+++ fileuploader-kiwiirc-plugin/src/token-manager.js
@@ -1,116 +1,114 @@
-import isPromise from 'p-is-promise'
+/* global kiwi:true */
+import isPromise from 'p-is-promise';
+import CacheLoader from './cache-loader';
 
-import CacheLoader from './cache-loader'
+const seconds = 1000;
+const minutes = 60 * seconds;
 
-const seconds = 1000
-const minutes = 60 * seconds
+const ErrExtJwtUnsupported = new Error('EXTJWT unsupported on this server/gateway');
 
-const ERR_UNKNOWNCOMMAND = 421
-
-const ErrExtJwtUnsupported = new Error('EXTJWT unsupported on this server/gateway')
-
-const UNSUPPORTED_TTL = 5 * minutes
+const UNSUPPORTED_TTL = 5 * minutes;
 
 export default class TokenManager {
     constructor() {
-        this.unsupportedNetworks = new Map()
-        this.requestToken = this.requestToken.bind(this) // ?!?!
-        this.cacheLoader = new CacheLoader(this.requestToken, TokenManager.assertValid)
+        this.unsupportedNetworks = new Map();
+        this.requestToken = this.requestToken.bind(this); // ?!?!
+        this.cacheLoader = new CacheLoader(this.requestToken, TokenManager.assertValid);
     }
 
     get(network) {
         if (this.unsupportedNetworks.has(network)) {
             if (new Date() - this.unsupportedNetworks.get(network) < UNSUPPORTED_TTL) {
-                return false // don't retry EXTJWT on unsupported servers
+                return false; // don't retry EXTJWT on unsupported servers
             }
         }
 
-        const maybePromise = this.cacheLoader.get(network)
+        const maybePromise = this.cacheLoader.get(network);
 
         if (isPromise(maybePromise)) {
-            const tokenRecordPromise = maybePromise
+            const tokenRecordPromise = maybePromise;
             return tokenRecordPromise
                 .then(tokenRecord => tokenRecord.token)
                 .catch(err => {
                     if (err === ErrExtJwtUnsupported) {
-                        return false
+                        return false;
                     }
-                    throw err
-                })
+                    throw err;
+                });
         }
 
-        const tokenRecord = maybePromise
-        return tokenRecord.token
+        const tokenRecord = maybePromise;
+        return tokenRecord.token;
     }
 
     async requestToken(network) {
-        const thisTokenManager = this
+        const thisTokenManager = this;
 
-        const respPromise = awaitMessage(
-            network.ircClient,
-            message => {
-                if (message.command === String(ERR_UNKNOWNCOMMAND) && message.params[1].toUpperCase() === 'EXTJWT') {
-                    throw ErrExtJwtUnsupported
-                }
+        const respPromise = awaitToken({ timeout: 10 * seconds });
+        network.ircClient.raw('EXTJWT');
 
-                return message.command.toUpperCase() === 'EXTJWT' && message.params[0] === '*'
-            },
-            { timeout: 10 * seconds }
-        )
-
-        network.ircClient.raw('EXTJWT')
-
-        let resp
+        let token;
         try {
-            resp = await respPromise
+            token = await respPromise;
         } catch (err) {
             if (err === ErrExtJwtUnsupported) {
-                const unsupportedAt = new Date()
-                thisTokenManager.unsupportedNetworks.set(network, unsupportedAt)
-                console.debug('Network does not support EXTJWT:', network)
+                const unsupportedAt = new Date();
+                thisTokenManager.unsupportedNetworks.set(network, unsupportedAt);
+                console.debug('Network does not support EXTJWT:', network);
             }
-            throw err
+            throw err;
         }
 
-        const acquiredAt = new Date()
-        const [target, token] = resp.params
-        return { token, acquiredAt }
+        const acquiredAt = new Date();
+        return { token, acquiredAt };
     }
 
     static assertValid(tokenRecord) {
-        const { token, acquiredAt } = tokenRecord
-        const now = new Date()
+        // eslint-disable-next-line no-unused-vars
+        const { token, acquiredAt } = tokenRecord;
+        const now = new Date();
         if (now - acquiredAt > 15 * seconds) {
-            throw new Error(`Stale token: ${(now - acquiredAt) / 1000} seconds age exceeds 15 second limit`)
+            throw new Error(`Stale token: ${(now - acquiredAt) / 1000} seconds age exceeds 15 second limit`);
         }
     }
 }
 
-function awaitMessage(ircClient, matcher, { timeout } = { timeout: undefined }) {
-    const { connection } = ircClient
+function awaitToken({ timeout } = { timeout: undefined }) {
     return new Promise((resolve, reject) => {
-        let timeoutHandle
+        let timeoutHandle;
+
+        const removeEvents = () => {
+            if (timeoutHandle) {
+                clearTimeout(timeoutHandle);
+            }
+            kiwi.off('irc.raw.421', rawHandler);
+            kiwi.off('irc.raw.EXTJWT', rawHandler);
+        };
+
+        const rawHandler = (command, event) => {
+            if (command === '421') {
+                if (event.params[1].toUpperCase() === 'EXTJWT') {
+                    event.handled = true;
+                    removeEvents();
+                    reject(ErrExtJwtUnsupported);
+                }
+                return;
+            }
+
+            event.handled = true;
+            const token = event.params[event.params.length - 1];
+            removeEvents();
+            resolve(token);
+        };
+
         if (timeout) {
             timeoutHandle = setTimeout(() => {
-                connection.removeListener('message', callback)
-                reject(new Error('Timeout expired'))
-            }, timeout)
+                removeEvents();
+                reject(new Error('Timeout expired'));
+            }, timeout);
         }
 
-        const callback = message => {
-            try {
-                if (matcher(message)) {
-                    connection.removeListener('message', callback)
-                    if (timeoutHandle) clearTimeout(timeoutHandle)
-                    resolve(message)
-                }
-            } catch (err) {
-                connection.removeListener('message', callback)
-                if (timeoutHandle) clearTimeout(timeoutHandle)
-                reject(err)
-            }
-        }
-
-        connection.on('message', callback)
-    })
+        kiwi.on('irc.raw.421', rawHandler);
+        kiwi.on('irc.raw.EXTJWT', rawHandler);
+    });
 }
